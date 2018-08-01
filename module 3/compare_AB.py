8b71c742acb6a1d3eeefec13decf64ed6442d65d
saad181 <saadahmed181@msitprogram.net>
2018-08-01 19:17:36 +0530

    compare_AB

---
+++ module 3/compare_AB.py
@@ -0,0 +1,12 @@
+varA=int(input())
+varB=int(input())
+if type(varA)==type(varB):
+	print("strings involved")
+elif varA>varB:
+    print("bigger")
+elif varA<varB: 
+    print("smaller")
+else:
+    print("equal")
+    
+      
