b8c830edd2fdc287141f48d88d6904b2f1f5aee0
saad181 <saadahmed181@msitprogram.net>
2018-08-04 13:05:45 +0530

    " -> 1 of 7 passed. pylint: 9.09/10 "

--- module 6/p2/special_char.py
+++ module 6/p2/special_char.py
@@ -1,4 +1,5 @@
 '''
+''
 Replace all the special characters(!, @, #, $, %, ^, &, *) in a given string with a space.
 example : ab!@#cd is the input, the output is ab   cd
 Output has three spaces, which are to be replaced with these special characters
@@ -7,7 +8,16 @@ def main():
     '''
     Read string from the input, store it in variable str_input.
     '''
-    str_input = input()
-
+    string1 = input()
+    string2 = " "
+    for char in string1:
+        if char in '!,@,#,$,%,^,&,*,':
+            string1 = string2
+            print(" ")
+        else:
+            string2 = string2 + char
+            print(string2)
+        #string3 = string1 + string2
+        #print(string3)
 if __name__ == "__main__":
     main()
--- module 6/p3/digit_product.py
+++ module 6/p3/digit_product.py
@@ -8,7 +8,14 @@ def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
-    int_input = int(input())
+    product = int(input())
+    mul = 0
+    for product in range (0, product, 1):
+        mul = mul*i 
+        i += 1
+        print(sum)
+
+
 
 
 if __name__ == "__main__":
