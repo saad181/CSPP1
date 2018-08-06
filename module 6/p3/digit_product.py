8a8d116267f14441827f9a00ac392c085343da4f
saad181 <saadahmed181@msitprogram.net>
2018-08-07 01:25:16 +0530

    " -> 0 of 10 passed. pylint: 10.00/10 "

--- module 6/p3/digit_product.py
+++ module 6/p3/digit_product.py
@@ -9,11 +9,9 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     product = int(input())
-    mul = 0
-    for product in range (0, product, 1):
-        mul = mul*i 
-        i += 1
-        print(sum)
+    
+
+    
 
 
 
---
+++ module 7/Functions - Assignment-1/assignment1.py
@@ -0,0 +1,40 @@
+# Functions | Assignment-1 - Paying Debt off in a Year
+'''program for balance'''
+#Write a program to calculate the credit card balance after one year
+#if a person only pays the minimum monthly payment required by the
+# credit card company each month.
+# The following variables contain values as described below:
+# balance - the outstanding balance on the credit card
+# annualInterestRate - annual interest rate as a decimal
+# monthlyPaymentRate - minimum monthly payment rate as a decimal
+#For each month, calculate statements on the monthly payment and remaining balance.
+#At the end of 12 months, print out the remaining
+# balance. Be sure to print out no more than two decimal digits of accuracy - so print
+# Remaining balance: 813.41
+# instead of
+# Remaining balance: 813.4141998135
+#So your program only prints out one thing: the remaining balance at
+#the end of the year in the format:
+# Remaining balance: 4784.0
+# A summary of the required math is found below:
+# Monthly interest rate= (Annual interest rate) / 12.0
+# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+# Updated balance each month = (Monthly unpaid balance) +
+# (Monthly interest rate x Monthly unpaid balance)
+def payingdebt(balance, annual_intrest_rate, monthly_payment_rate):
+    '''this function calculates the credit card balance '''
+    for _ in range(12):
+        monthly_intrest_rate = (annual_intrest_rate)/12.0
+        minimum_monthly_payment = (monthly_payment_rate)*(balance)
+        monthly_unpaid_balance = (balance)-(minimum_monthly_payment)
+        balance = (monthly_unpaid_balance) + (monthly_intrest_rate*monthly_unpaid_balance)
+    return round(balance, 2)
+def main():
+    '''this function is to input the data'''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print(payingdebt(data[0], data[1], data[2]))
+if __name__ == "__main__":
+    main()
