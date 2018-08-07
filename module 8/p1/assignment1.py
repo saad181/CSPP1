791d41b61c8165e402a99f050414a15982027905
saad181 <saadahmed181@msitprogram.net>
2018-08-07 17:42:46 +0530

    " -> 10 of 10 passed. pylint: 9.17/10 "

--- module 8/p1/assignment1.py
+++ module 8/p1/assignment1.py
@@ -1,23 +1,22 @@
 # Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
-
+'''program to print the factorial of  number'''
+# Write a Python function, factorial(n),
+#that takes in one number and returns the factorial of given number.
 # This function takes in one number and returns one number.
-
-
-def factorial(n):
+def factorial(num):
     '''
     n is positive Integer
-
-    returns: a positive integer, the factorial of n.
+ returns: a positive integer, the factorial of n.
     '''
     # Your code here
-    pass
-    
-
-
+    temp = num
+    if temp == 0:
+        return 1
+    if num == 1:
+        return num
+    return num*factorial(num-1)
 def main():
-    a = input()
-    print(factorial(int(a)))    
-
-if __name__== "__main__":
+    a_b = input()
+    print(factorial(int(a_b)))
+if __name__ == "__main__":
     main()
