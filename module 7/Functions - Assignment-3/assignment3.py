fb2862ec070470720d8485b3529120c3cbf81e80
saad181 <saadahmed181@msitprogram.net>
2018-08-11 16:40:33 +0530

    " -> 0 of 10 passed. pylint: 10.00/10 "

--- module 7/Functions - Assignment-3/assignment3.py
+++ module 7/Functions - Assignment-3/assignment3.py
@@ -72,7 +72,7 @@ def main():
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    return payingdebtoffinayear(data[0], data[1])
+    print(payingdebtoffinayear(data[0], data[1]))
 
 if __name__ == "__main__":
     main()
