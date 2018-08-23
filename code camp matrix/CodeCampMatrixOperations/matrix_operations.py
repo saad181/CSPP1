d01ac957e6e8f0841be3ae5accd4eea84fdfd491
saad181 <saadahmed181@msitprogram.net>
2018-08-23 20:18:08 +0530

    " -> 6 of 8 passed. pylint: 9.58/10 "

--- code camp matrix/CodeCampMatrixOperations/matrix_operations.py
+++ code camp matrix/CodeCampMatrixOperations/matrix_operations.py
@@ -1,4 +1,6 @@
-def mult_matrix(m1, m2):
+import copy
+'''Addition and multiplication of matrices'''
+def mult_matrix(m_1, m_2, temp_1, temp_2):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -6,9 +8,22 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    pass
+    if temp_1 != temp_2:
+        print("Error: Matrix shapes invalid for mult")
+        return None
+    mul = []
+    for itemp in range(0, len(m_1), 1):
+        temp = []
+        for jtemp in range(len(m_2[0])):
+            res = 0
+            for ktemp in range(0, len(m_2), 1):
+                res += int(m_1[itemp][ktemp]) * int(m_2[ktemp][jtemp])
+            temp.append(res)
+        mul.append(temp)
+    return mul
 
-def add_matrix(m1, m2):
+
+def add_matrix(m_1, m_2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -16,9 +31,20 @@ def add_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    pass
+    len_1 = len(m_1)
+    len_2 = len(m_2)
+    if len_1 != len_2:
+        print("Error : Matrix shapes invalid for addition ")
+        return None
+    add = copy.deepcopy(m_1)
+    for itemp in range(0, len_2, 1):
+        for jtemp in range(0, len(m_2[itemp]), 1):
+            add[itemp][jtemp] = int(add[itemp][jtemp])
+            add[itemp][jtemp] += int(m_2[itemp][jtemp])
+    return add
 
-def read_matrix():
+
+def read_matrix(size):
     '''
         read the matrix dimensions from input
         create a list of lists and read the numbers into it
@@ -26,9 +52,16 @@ def read_matrix():
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    pass
-
+    total = 0
+    matrix = []
+    val1 = int(size[0])
+    for _ in range(0, val1, 1):
+        row = input().split()
+        matrix.append(row)
+        total += len(row)
+    return matrix, total
 def main():
+    '''main function for addition and multiplication'''
     # read matrix 1
 
     # read matrix 2
@@ -36,7 +69,16 @@ def main():
     # add matrix 1 and matrix 2
 
     # multiply matrix 1 and matrix 2
-    pass
+    size1 = input().split(',')
+    m_1, total1 = read_matrix(size1)
+    size2 = input().split(',')
+    m_2, total2 = read_matrix(size2)
+    if total1 != total2:
+        print("Error: Invalid input for the matrix")
+    else:
+        print(add_matrix(m_1, m_2))
+        print(mult_matrix(m_1, m_2, size1[1], size2[0]))
+
 
 if __name__ == '__main__':
     main()
