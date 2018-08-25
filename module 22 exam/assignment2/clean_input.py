657a0bce2614635a71b4918d6645a4001431a7ae
saad181 <saadahmed181@msitprogram.net>
2018-08-25 12:14:24 +0530

    " -> 3 of 3 passed. pylint: 8.18/10 "

--- module 22 exam/assignment2/clean_input.py
+++ module 22 exam/assignment2/clean_input.py
@@ -1,14 +1,19 @@
 '''
-Write a function to clean up a given string by removing the special characters and retain 
+Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
-
+import re
 def clean_string(string):
-    pass
-
+    ''' clean the string by removing the special characters'''
+    characters = ""
+    clean = re.sub("\W+", '', string)
+    characters = clean.replace(" ", "")
+    return characters
 def main():
+    ''' This main fuction is used to call the clean string'''
     string = input()
     print(clean_string(string))
-
 if __name__ == '__main__':
     main()
+#https://stackoverflow.com/questions/5843518/
+      #remove-all-special-characters-punctuation-and-spaces-from-string
