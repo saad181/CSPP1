a3ac8a18a41a00917417810d5b7d1f4900aabef4
saad181 <saadahmed181@msitprogram.net>
2018-12-21 12:02:13 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 3/10 "

--- remedial exam/CSPP-1_Question/Sudoku.py
+++ remedial exam/CSPP-1_Question/Sudoku.py
@@ -1,34 +1,64 @@
-def create(g, row, col):
-    lis= set()
-    for i in range(9):
-        if g[row][i] != '0':
-            lis.add(g[row][i])
-        if g[i][col] != '0':
-            lis.add(g[i][col])
-    return lis
+def check(sudoku):
+    if (sudoku.count('.') == 0):
+        return 'Given sudoku is solved'
 
-def valid(g):
-    for i in range(9):
-        for j in range(9):
-            res = ""
-            s = set()
-            if g[i][j] == '0':
-                s = create(g, i, j)
-                # print(s)
-            if len(s) != 0:
-                for each in "123456789":
-                    if each not in s:
-                        res += each
-                print(res)
+    elif(len(sudoku) != 81):
+        return 'Invalid input'
 
-if __name__=="__main__": 
-      #for 2D array
-    grid=[['0' for x in range(9)]for y in range(9)] 
-    given_input = input()
-    k = 0
-    for i in range(9):
-        for j in range(9):
-            if given_input[k] != '.':
-                grid[i][j] = given_input[k]
-            k += 1
-    valid(grid)
+    else:
+        collection_rows = []
+        for i in range(0,len(sudoku),9):
+            row_ = [int(j) for j in sudoku[i:i+9] if j != '.']
+            set_row = {k for k in row_}
+            if len(row_) != len(set_row):
+                return 'Invalid Sudoku:Duplicate values'
+            elif collection_rows.count(row_) == 1:
+                return 'Invalid Sudoku:Duplicate values'
+            else:
+                collection_rows.append(row_)
+        return ''
+
+def possiblities(sudoku):
+    check= [False]*9
+    count = 0
+    i = ''
+    for i in sudoku:
+        if count == 9:
+            # print(check)
+            for j in range(9):
+                if check[j] == False:
+                    print(j+1)
+            check = [False]*9
+            # print(check)
+            count = 0
+        #   count = count + 1
+        #   if(i == '.'):
+        #       continue
+        #   check[int(i) - 1] = True
+        # else:
+        count = count + 1
+        if(i == '.'):
+            continue
+        check[int(i) - 1] = True
+    if count == 9:
+            # print(check)
+            for j in range(9):
+                if check[j] == False:
+                    print(j+1)
+            check = [False]*9
+            # print(check)
+            count = 0
+
+
+def main():
+    sudoku_ = input()
+    check_result = check(sudoku_)
+    if check_result != '':
+        print(check_result)
+        return
+    possiblities(sudoku_)
+        
+
+
+if _name_ == "_main_":
+    main()
