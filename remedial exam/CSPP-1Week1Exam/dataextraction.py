cbeb19e23981e8ac399a577842e2522fcee6958b
saad181 <saadahmed181@msitprogram.net>
2018-12-29 12:28:55 +0530

    "cspp1_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- remedial exam/CSPP-1Week1Exam/dataextraction.py
+++ remedial exam/CSPP-1Week1Exam/dataextraction.py
@@ -1,19 +1,23 @@
 def getImage(image):
-	string = image.split("src")
+	string = image.split("<img")
 	count = 0
-	start = "./webpage5"
-	end = ".png"
+	start = "./webpage5\""
+	end = ".jpg\""
 	for eachthing in string:
-		if "./webpage5" in eachthing:
+
+		if start in eachthing:
 			try:
-				pos = eachthing.index(start)
-				eachthing = eachthing[pos+length(start): end]
-				ending = eachthing.index(end)
+				possible = eachthing.index(start)
 				count = count+1
+				eachthing = eachthing[length(start): end]
+				ending = eachthing.index(end)
+				# count = count+1
+
 			except:
 				pass
 			else:
-				print(eachthing[:ending])		
+				print(eachthing[:end])
+
 
 
 
@@ -25,7 +29,7 @@ def getImage(image):
 def main():
     file = open("webpage5.html", errors="ignore").read()
     getImage(file)
-    
+    # print(file)
    
 if __name__ == '__main__':
     main()
