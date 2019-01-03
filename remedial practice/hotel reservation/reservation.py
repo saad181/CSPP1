c3687b292ea683e0bf0828ae1639305afd02d786
saad181 <saadahmed181@msitprogram.net>
2019-01-03 18:46:33 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/10 "

--- remedial practice/hotel reservation/reservation.py
+++ remedial practice/hotel reservation/reservation.py
@@ -1,40 +1,24 @@
-def main():
-    inp = int(input())
-    l = []
-    for i in range(inp):
-        tokens = input().split(" ")
-        if tokens[0] == "reserve":
-            getReserve(tokens[1])
-        elif tokens[0] == "reserveN":
-            getN(tokens[1], tokens[2])
-        elif tokens[0] == "build":
-            getBuild()
-        elif tokens[0] == "print":
-            getPrinting()
-if  __name__ == '__main__':
-        main()  
 
-# def getReserve(person):
-#     roomnum = 0
-#     dictionary = {}
-#     lis = []
-#     total_rooms = 5
-#     roomnum = roomnum+1
-#     if roomnum>total_rooms:
-#         print("All Rooms are reserved")
-#         return
-#     if roomnum not in dictionary.values():
-#         dictionary[person] = roomnum
-#         lis.append(roomnum)
-#         print(person+" "+ roomnum)
-#     else:
-#         reserve(person)    
+def getReserve(person):
+    roomnum = 0
+    dictionary = {}
+    lis = []
+    total_rooms = 5
+    roomnum = roomnum+1
+    if roomnum>total_rooms:
+        print("All Rooms are reserved")
+        return
+    if roomnum not in dictionary.values():
+        dictionary[person] = roomnum
+        lis.append(roomnum)
+        print(person+" "+ roomnum)
+    else:
+        reserve(person)    
 
 
 
 
     
-
 def getN(person, roomnum):
     total_rooms = 5
     for room in sorted(lis):
@@ -56,6 +40,21 @@ def getBuild(extraroom):
     total_rooms = 5
     print("Added" + str(extraroom)+ "more rooms")
     total_rooms = total_rooms+extraroom
-    
 
 
+
+def main():
+    inp = int(input())
+    l = []
+    for i in range(inp):
+        tokens = input().split(" ")
+        if tokens[0] == "reserve":
+            getReserve(tokens[1])
+        elif tokens[0] == "reserveN":
+            getN(tokens[1], tokens[2])
+        elif tokens[0] == "build":
+            getBuild()
+        elif tokens[0] == "print":
+            getPrinting()
+if  __name__ == '__main__':
+        main()  
