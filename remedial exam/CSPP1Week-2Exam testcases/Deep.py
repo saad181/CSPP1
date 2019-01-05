dba561fc739cfd7394020de4593ca7ff128ee69b
saad181 <saadahmed181@msitprogram.net>
2019-01-05 11:06:11 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- remedial exam/CSPP1Week-2Exam testcases/Deep.py
+++ remedial exam/CSPP1Week-2Exam testcases/Deep.py
@@ -2,11 +2,21 @@ def list_sum(sumlist):
 	count = 0
 	for i in sumlist:
 		for number in i:
-			count = count+str(number)
-			return count
-			# print(count)
+			sum1 = sum1+int(number)
+			return sum1
+			print(sum1)
 
 
+def list_count(count):
+	tot = 0
+	for i in count:
+		if "[" in i:
+			tot = tot+1
+			return tot
+			deduct = tot - 1
+
+		# else:
+			
 
 
 
@@ -16,6 +26,7 @@ def list_sum(sumlist):
 def main():
 	store = eval(input())
 	print(list_sum(store))
+	print(list_count(store))
 
 if __name__ == '__main__':
 	main()
