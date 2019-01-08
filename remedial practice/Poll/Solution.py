abf6fd62ceca4875101b429adc2a0251c3d8766b
saad181 <saadahmed181@msitprogram.net>
2019-01-08 20:04:41 +0530

    "cspp2_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- remedial practice/Poll/Solution.py
+++ remedial practice/Poll/Solution.py
@@ -1,48 +1,32 @@
-def onlyoneqn(listt):
-    a = {}
-    k = []
-    v = []
-    for i in listt:
-        if i in a:
-            a[i] += 1
-        else:
-            a[i] = 1
-    k = list(a.keys())
-    v = list(a.values())
-    return "Highest number of votes for question : Who should be the next Prime Minister? :"+" "+k[v.index(max(v))]
+def equation():
+	options = []
+	partcipant = []
+	d = {}
+
+	for i in inp:
+		
+		for i in options:
+			options.append(i)
 def main():
-    numberofquestions = int(input())
+    ques = int(input())
     d = dict()
-    qnslist = []
-    for qns in range(0, numberofquestions):
-        qntext = input()
-        qnslist.append(qntext)
-        newlist = []
-        for options in range(0, 4):
-            string = input()  
-            newlist.append(string)
-        d[qntext] = newlist
-    nl = []
-    dictt = dict()
+    questionlist= []
+    for qns in range(0, ques):
+        questiontext = input()
+        questionlist.append(questiontext)
+        new = []
+        for options in range(0,4):
+            choice = input()
+            new.append(choice)
+        d[questiontext] = new
+    new1 = []
+    dictionary = dict()
     optionslist = []
-    newl = []
-    num = int(input())
-    flatten = []
-    for voters in range(num):
-        p = input()
-        nl.append(p)
-        optionslist = []
-        for k in range(numberofquestions):
-            b = input().split(" ")
-            optionslist.append(b[len(b) - 1])
-        dictt[p] = optionslist
-        newl.append(optionslist)
-    for items in newl:
-        for j in items:
-            flatten.append(j)
-    if len(flatten) == 3:
-        print(onlyoneqn(flatten))
-    else:
-              pass
-if __name__ == '__main__':
-    main()
+    print("s..........................")
+
+
+
+		
+
+
+
