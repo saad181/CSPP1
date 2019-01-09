3b14a87b721eb8f2d38a88bf94c75ddf3aed4505
saad181 <saadahmed181@msitprogram.net>
2019-01-09 22:41:20 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ remedial practice/Assignment-1/Solution.py
@@ -0,0 +1,9 @@
+def main():
+	inp = input()
+	split = inp.split(" ")
+	sp = int(split[1])
+	print("|----------------|")
+	print("| Load Questions |")
+	print("|----------------|")
+	print(str(sp) " " + "are added to the quiz")
+main()
