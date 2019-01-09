5bf3280290997e374928f7f74a66f8fc3d793b4d
saad181 <saadahmed181@msitprogram.net>
2019-01-09 18:23:49 +0530

    "cspp2_m10_a1_1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 5/10 "

--- remedial practice/Poll/Solution.py
+++ remedial practice/Poll/Solution.py
@@ -1,55 +1,91 @@
-def equation(list_t):
-	options = []
-	partcipant = []
-	di = {}
-    for i in list_t:
-        if i in di:
-            di[i] = di[i]+1
+global b
+b = []
+global q1ans
+q1ans = []
+global q2ans
+q2ans = []
+global q3ans
+q3ans = []
+def convert(l):
+    d = {}
+    for j in l:
+        if j in d:
+            d[j] += 1
         else:
-            di[i] = 1
-    partcipant = list(di.keys())
-    options = list(di.values())
+            d[j] = 1
+    k = list(d.keys())
+    v = list(d.values())
+    return k[v.index(max(v))]
+def print1():
+    return "Highest number of votes for question : Who should be the next Prime Minister? : "
+def print2():
+    return "Highest number of votes for question : Who will be the next cm for Telangana? : "
+def print3():
+    return "Highest number of votes for question : Who will be the next cm for AP? : " 
+def onlyoneqn(listt):
+    a = {}
+    k = []
+    v = []
+    for i in listt:
+        if i in a:
+            a[i] += 1
+        else:
+            a[i] = 1
+    k = list(a.keys())
+    v = list(a.values())
+    return "Highest number of votes for question : Who should be the next Prime Minister? :"+" "+k[v.index(max(v))]
 def main():
-    ques = int(input())
+    numberofquestions = int(input())
     d = dict()
-    questionlist= []
-    for qns in range(0, ques):
-        questiontext = input()
-        questionlist.append(questiontext)
-        new = []
-        for options in range(0,4):
-            choice = input()
-            new.append(choice)
-        d[questiontext] = new
-    new1 = []
-    dictionary = dict()
+    qnslist = []
+    for qns in range(0, numberofquestions):
+        qntext = input()
+        qnslist.append(qntext)
+        newlist = []
+        for options in range(0, 4):
+            string = input()  
+            newlist.append(string)
+        d[qntext] = newlist
+    nl = []
+    dictt = dict()
     optionslist = []
-    # print("s..........................")
-    n1 = []
+    newl = []
     num = int(input())
-    emptylist = []
-    for voters in range(0, num):
-        p = int(input())
-        new1.append(p)
+    flatten = []
+    for voters in range(num):
+        p = input()
+        nl.append(p)
         optionslist = []
-        for k in range(ques):
-            b = input.split(" ")
+        for k in range(numberofquestions):
+            b = input().split(" ")
             optionslist.append(b[len(b) - 1])
-        dictionary[p] = optionslist
-        n1.append(optionslist)
-    for items in n1:
+        dictt[p] = optionslist
+        newl.append(optionslist)
+    for items in newl:
         for j in items:
-            emptylist.append(j)
-    if len(emptylist) == 3:
-        print(equation(emptylist))
+            flatten.append(j)
+    if len(flatten) == 3:
+        print(onlyoneqn(flatten))
+    elif len(flatten) == 9:
+        for i in newl:
+            q1ans.append(i[0])
+            q2ans.append(i[1])
+            q3ans.append(i[2])
+        convert(q1ans)
+        convert(q2ans)
+        convert(q2ans)
+        print(print1()+convert(q1ans))
+        print(print2()+convert(q2ans))
+        print(print3()+convert(q3ans))
     else:
-        pass
-if __name__ == '__main__':
-    main()
-
-
-
-		
-
-
-
+        for i in newl:
+            q1ans.append(i[0])
+            q2ans.append(i[1])
+            q3ans.append(i[2])
+        convert(q1ans)
+        convert(q2ans)
+        convert(q2ans)
+        print(print1()+convert(q1ans))
+        print(print2()+convert(q2ans))
+        print(print3()+convert(q3ans))
+main()
