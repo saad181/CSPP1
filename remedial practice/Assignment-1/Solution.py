2348036b76c2a3a8f244a744a0141d277cd9a8f1
saad181 <saadahmed181@msitprogram.net>
2019-01-10 11:39:03 +0530

    "m19_a1: 0/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- remedial practice/Assignment-1/Solution.py
+++ remedial practice/Assignment-1/Solution.py
@@ -1,33 +1,13 @@
-def lickme(l):
-	qns = l[0]
-	choices = l[1]
-	correctchoice = l[2]
-	maxmarks = l[3]
-	penalty = l[4]
-	ql = qns.split(" ")
-	srno = ql[2]
-	print(qns+"("+srno+")")
-	temp = choices.split(",")
-	for i in range(0, len(temp)):
-		print(temp[i], end="\t")
-		if i == 'choice 4':
-			print(i, end="")
-def fuckme(strings):
-	listt = strings.split(":")
-	lickme(listt)
 def main():
 	inp = input()
-	split = inp.split(" ")
-	sp = int(split[1])
+	token = inp.split(" ")
+	sp = int(token[1])
 	print("|----------------|")
 	print("| Load Questions |")
 	print("|----------------|")
-	print(str(sp)+" "+"are added to the quiz")
+	print(sp+" "+"are added to the quiz")
 	print("|------------|")
 	print("| Start Quiz |")
 	print("|------------|")
-	for i in range(0,sp):
-		st = input()
-		fuckme(st)
-		print()
-main()
+	
+
