f13b45ff5ceb1e28a044a3d6ba108459b3daf870
saad181 <saadahmed181@msitprogram.net>
2019-01-12 10:02:33 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ remedial exam/CSPP1-Week3Exam/Quiz.py
@@ -0,0 +1,25 @@
+def quiz(l):
+	token = l.split("|")
+	stu_id  = token[0]
+	ques = token[1]
+	response = token[2]
+	answer = token[3]
+	points = token[4]
+	li = []
+	li.append(stu_id)
+	print(li)
+
+
+
+
+
+
+
+
+
+def main():
+	inp = int(input())
+	for i in range(0,inp):
+		line = input()
+		quiz(line)
+main()
