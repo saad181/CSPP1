752cec52007cc69c6d93794f35c4cf8fdb156c96
saad181 <saadahmed181@msitprogram.net>
2019-01-24 12:39:24 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

--- remedial exam/CSPP1-Week3Exam/Quiz.py
+++ remedial exam/CSPP1-Week3Exam/Quiz.py
@@ -1,41 +1,41 @@
 def quiz(l):
-	token = l.split("|")
-	stu_id  = token[0]
-	ques = token[1]
-	response = token[2]
-	answer = token[3]
-	points = int(token[4])
-	total = dict()
-	marksobtained = dict()
-	try:
-		if points == 'a':
-			raise Exception("invalid points")
-		else:
-			if stu_id not in marksobtained:
-				total[stu_id] = int(points)
-				marksobtained[stu_id] = int(0)
-			else:
-				total[stu_id] = total[stu_id]+(points)
-			if response == answer:
-				marksobtained[stu_id] += points
-			else:
-				marksobtained[stu_id] -= points
-		for keys in sorted(marksobtained):
-			for each in sorted(total):
-				if keys == each:
-					score = (marksobtained[keys]/total[each] * 100)
-					if score <=0:
-						score = 0.0
-						print(each + ": "+ str(score)+"%")
-					else:
-						print(each + ": "+ str(float(score))+ "%")
-	except Exception as e:
-		print(e)
+    token = l.split("|")
+    stu_id  = token[0]
+    ques = token[1]
+    response = token[2]
+    answer = token[3]
+    points = int(token[4])
+    total = dict()
+    marksobtained = dict()
+    try:
+        if points == 'a':
+            raise Exception("invalid points")
+        else:
+            if stu_id not in marksobtained:
+                total[stu_id] = int(points)
+                marksobtained[stu_id] = int(0)
+            else:
+                total[stu_id] = total[stu_id]+ points
+            if response == answer:
+                marksobtained[stu_id] += points
+            else:
+                marksobtained[stu_id] -= points
+        for keys in sorted(total):
+            for each in sorted(marksobtained):
+                if keys == each:
+                    score = int(marksobtained[each]/total[keys] * 100)
+                    if score <=0:
+                        score = 0.0
+                        print(each + ": "+ str(score)+"%")
+                    else:
+                        print(each + ": "+ str(float(score))+ "%")
+    except Exception as e:
+        print(e)
 
-	
+    
 def main():
-	inp = int(input())
-	for i in range(0,inp):
-		line = input()
-		quiz(line)
+    inp = int(input())
+    for i in range(0,inp):
+        line = input()
+        quiz(line)
 main()
