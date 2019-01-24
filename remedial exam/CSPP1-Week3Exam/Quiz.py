6a817ce9d4f7a14ce0d0f3591e3a5d14f1b091cf
saad181 <saadahmed181@msitprogram.net>
2019-01-24 11:47:19 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- remedial exam/CSPP1-Week3Exam/Quiz.py
+++ remedial exam/CSPP1-Week3Exam/Quiz.py
@@ -5,25 +5,44 @@ def quiz(l):
 	response = token[2]
 	answer = token[3]
 	points = int(token[4])
-	count = 0
-	d = dict()
-	li = list()
-	li.append(response,answer,points)
-	k = d.keys()
-	v = d.values(li)
-	for k,v in sorted(d):
-		if response == answer:
-			count = count+ points
-			total_score = (points/count)*100
-	print((stu_id)+":"+" "+int(total_score))
-	# else:
-	# 	deduct = (points)
+	total = dict()
+	marksobtained = dict()
+	try:
+		if points == 'a':
+			raise Exception("invalid points")
+		else:
+			if stu_id not in marksobtained:
+				total[stu_id] = int(points)
+				marksobtained[stu_id] = int(0)
+			else:
+				total[stu_id] = total[stu_id]+(points)
+			if response == answer:
+				marksobtained[stu_id] += points
+			else:
+				marksobtained[stu_id] -= points
+				if marksobtained[stu_id] < 0:
+					marksobtained[stu_id] += points
+				else:
+					marksobtained[stu_id]
+		for keys in sorted(marksobtained):
+			for each in sorted(total):
+				if keys == each:
+					score = (marksobtained[keys]/total[each] * 100)
+					if score <=0:
+						score = 0.0
+						print(each + ": "+ str(score)+"%")
+					else:
+						print(each + ": "+ str(float(score))+ "%")
+	except Exception as e:
+		print(e)
 
-
-
+	
 def main():
 	inp = int(input())
 	for i in range(0,inp):
 		line = input()
 		quiz(line)
 main()
+
+
+
